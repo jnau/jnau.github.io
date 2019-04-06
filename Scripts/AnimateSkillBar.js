8bb3b1ebb8a3dbc24c5a61b2c4d5f3c91bc2f6a7
Jessica Au <jnau@ucdavis.edu>
2019-04-05 20:44:26 -0700

    Update AnimateSkillBar.js

--- Scripts/AnimateSkillBar.js
+++ Scripts/AnimateSkillBar.js
@@ -1,6 +1,6 @@
-
-	$(document).ready(function() {
-		$('.skillbar').each(function(){
-			$(this).find('skillbar-bar').animate({width:$(this).attr.('data-percent')},6000);
-	});
+$(document).ready(function() {
+	$('.skillbar').each(function(){
+		$(this).find('skillbar-bar').animate({width:$(this).attr('data-percent')}
+			,6000);
+});
 
