a000df99b5d40c2a22ede009b42d9c4f2d542014
Jessica Au <jnau@ucdavis.edu>
2019-04-05 20:09:15 -0700

    Update AnimateSkillBar.js

--- AnimateSkillBar.js
+++ AnimateSkillBar.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({width:jQuery(this).attr('data-percent')},6000);
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
 	});
 });
