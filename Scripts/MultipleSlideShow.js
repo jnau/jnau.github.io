1cb040c501c107d1f61d72f4cbab29fd4f697d3c
Jessica Au <jnau@ucdavis.edu>
2019-04-08 00:49:51 -0700

    Update MultipleSlideShow.js

--- Scripts/MultipleSlideShow.js
+++ Scripts/MultipleSlideShow.js
@@ -11,10 +11,10 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
+  if (n > x.length) {slideIndex[no] = 2}    
+  if (n < 2) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no]-2].style.display = "block";  
 }
