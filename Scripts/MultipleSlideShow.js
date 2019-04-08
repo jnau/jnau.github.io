a282a0a59667b61597fa6360361803855b5bed2b
Jessica Au <jnau@ucdavis.edu>
2019-04-08 00:39:15 -0700

    Added more slideshows

--- Scripts/MultipleSlideShow.js
+++ Scripts/MultipleSlideShow.js
@@ -1,7 +1,8 @@
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
+var slideIndex = [1,2];
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
