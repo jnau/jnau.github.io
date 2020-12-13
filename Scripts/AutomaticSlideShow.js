ba4dc44d07fec53b65fe2ce83768b3c39e920a5b
Jessica Au <jessica.au10@gmail.com>
2020-12-13 13:10:22 -0800

    removed automatic slideshow

--- Scripts/AutomaticSlideShow.js
+++ Scripts/AutomaticSlideShow.js
@@ -1,10 +1,12 @@
-var slideIndex = 0;
+var slideIndex = 1;
 showSlides();
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
