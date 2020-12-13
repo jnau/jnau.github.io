fd248791df977db564be23d8d870dc73ff9759f3
Jessica Au <jessica.au10@gmail.com>
2020-12-13 12:58:28 -0800

    Update AutomaticSlideShow.js

--- Scripts/AutomaticSlideShow.js
+++ Scripts/AutomaticSlideShow.js
@@ -4,11 +4,16 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 10000); // Change image every 10 seconds
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 20000); // Change image every 10 seconds
 }
